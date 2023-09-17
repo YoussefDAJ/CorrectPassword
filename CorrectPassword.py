4d1adc582502815fe3f365eafaabad9a4babc102
Youssef <youssefelbaye@gmail.com>
2023-09-17 20:45:37 +0200

    adding the mean code

---
+++ CorrectPassword.py
@@ -0,0 +1,6 @@
+password = input("Please enter your password: \n")
+correct_password = "ELbayeYoussef"
+if password == correct_password:
+    print("Hello Youssef, good to see you again")
+else:
+    print("Sorry, you can't use the app")
---
+++ README.md
@@ -0,0 +1,2 @@
+Ce code est peut etre utiliser dans n'importe plateforme qui se sert d'un mot de pass.
+Vous pouvez changer le mot de pass en fonction de votre choix.
